d8a150f818bfb16940c03c354e937a80341636be
pietroking <pietrobking@gmail.com>
2023-01-22 06:25:01 -0300

    session

--- election-test/src/test/java/br/com/sicredi/election/aceitacao/session/PatchSessionTest.java
+++ election-test/src/test/java/br/com/sicredi/election/aceitacao/session/PatchSessionTest.java
@@ -7,8 +7,5 @@ import br.com.sicredi.election.service.SessionService;
 import br.com.sicredi.election.service.ZoneService;
 
 public class PatchSessionTest extends BaseTest {
-    ZoneService zoneService = new ZoneService();
-    ZoneBuilder zoneBuilder = new ZoneBuilder();
-    SessionService sessionService = new SessionService();
-    SessionBuilder sessionBuilder = new SessionBuilder();
+
 }
